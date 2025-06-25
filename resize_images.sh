3355a49f067a80659c8a25a0887db9983f97213b
Anders Eknert <anders@eknert.com>
2025-06-25 20:20:38 +0200

    Add Cheep Chomp
    Signed-off-by: Anders Eknert <anders@eknert.com>

--- resize_images.sh
+++ resize_images.sh
@@ -7,5 +7,3 @@ for file in assets/gallery/*.png; do
         magick "$file" -resize 300x300\> -strip -quality 85 "$file"
     fi
 done
-
-echo "Image processing complete! Original images are backed up in assets_backup/"
